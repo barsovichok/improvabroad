fcbd0a4b6c0cdf41b785bd7aac8e65f62eb22b73
t.kulagina <t.kulagina@sweedpos.com>
2024-07-08 00:04:43 +0200

    change deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://www.improv-abroad.com',
 	integrations: [
 		starlight({
 			title: 'Юмор по/на/ехавший',
